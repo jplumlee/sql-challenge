f13d95b81736924042dcdfaa08ee3a39ac5b25a5
Justan <jplumlee@outlook.com>
2020-08-21 19:40:10 -0700

    Updated Schema

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,34 +1,49 @@
 CREATE TABLE departments (
-	dept_no INT,
-	dept_name VARCHAR
+	dept_no VARCHAR,
+	dept_name VARCHAR,
+	PRIMARY KEY (dept_no)
 );
 
 CREATE TABLE dept_emp (
-	emp_no INT,
-	dept_no VARCHAR
+	emp_no SERIAL,
+	dept_no VARCHAR,
+	PRIMARY KEY (emp_no)
 );	
 
 CREATE TABLE dept_manager (
 	dept_no VARCHAR,
-	emp_no INT
+	emp_no SERIAL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE employees (
-	emp_no INT,
+	emp_no SERIAL,
 	emp_title_id VARCHAR,
-	birth_date VARCHAR,
+	birth_date DATE,
 	first_name VARCHAR,
 	last_name VARCHAR,
 	sex VARCHAR,
-	hire_date VARCHAR
+	hire_date DATE,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 CREATE TABLE salaries (
-	emp_no INT,
-	salary INT
+	emp_no SERIAL,
+	salary INT,
+	PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE titles (
 	title_id VARCHAR,
-	title VARCHAR
+	title VARCHAR,
+	PRIMARY KEY (title_id)
 );
+
+-- Create FKs
+ALTER TABLE dept_emp
+	ADD FOREIGN KEY (dept_no)
+	REFERENCES departments(dept_no)
+    MATCH SIMPLE
+;
